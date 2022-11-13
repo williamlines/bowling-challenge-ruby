8f07867ef9c6e317247179b2d790427e089dd86f
William Lines <williamlines42@gmail.com>
2022-11-13 13:16:11 +0000

    added roll_2 method to Frame

--- lib/frame.rb
+++ lib/frame.rb
@@ -11,13 +11,38 @@ class Frame
   end
 
   def roll_1(io = Kernel)
-    
+    io.puts "#{@pins} pins remaining"
     io.puts "Enter score for first roll"
-    @score1 = io.gets.chomp.to_i
+    user_input = io.gets.chomp.to_i
+    
+    raise 'error- score higher than remaining pins' if user_input > @pins
+
+    @score1 = user_input
+    @pins -= user_input
     
     if @score1 == 10
       @bonus = true
       @bonus_type = 'strike'
+    end
+  end
+
+  def roll_2(io = Kernel)
+    io.puts "#{@pins} pins remaining"
+    io.puts "Enter score for second roll"
+    user_input = io.gets.chomp.to_i
+
+    raise 'error- score higher than remaining pins' if user_input > @pins
+    
+    @score2 = user_input
+    @pins -= user_input
+
+    if @score2 == 10
+      @bonus = true
+      @bonus_type = 'strike'
+    
+    elsif @pins == 0
+      @bonus = true
+      @bonus_type = 'spare'
     end
   end
 end
--- spec/unit/frame_spec.rb
+++ spec/unit/frame_spec.rb
@@ -14,19 +14,97 @@ RSpec.describe Frame do
   context "roll_1 method" do
     it 'can take io input and score the roll when not a strike' do
       io = double(:io)
+      expect(io).to receive(:puts).and_return('10 pins remaining')
       expect(io).to receive(:puts).and_return('Enter score for first roll')
       expect(io).to receive(:gets).and_return('9')
       frame.roll_1(io)
       expect(frame.score1).to eq 9
     end
+    
     it 'sets @bonus to true and @bonus_type to strike on a strike' do
       io = double(:io)
+      expect(io).to receive(:puts).and_return('10 pins remaining')
       expect(io).to receive(:puts).and_return('Enter score for first roll')
       expect(io).to receive(:gets).and_return('10')
       frame.roll_1(io)
       expect(frame.score1).to eq 10
       expect(frame.bonus).to be true
+
       expect(frame.bonus_type).to eq 'strike'
+    end
+
+    it 'updates remaining pins' do
+      io = double(:io)
+      expect(io).to receive(:puts).and_return('10 pins remaining')
+      expect(io).to receive(:puts).and_return('Enter score for first roll')
+      expect(io).to receive(:gets).and_return('7')
+      frame.roll_1(io)
+      expect(frame.pins).to eq 3
+    end
+
+    it 'raises exeption if given an impossible roll' do
+      io = double(:io)
+      expect(io).to receive(:puts).and_return('10 pins remaining')
+      expect(io).to receive(:puts).and_return('Enter score for first roll')
+      expect(io).to receive(:gets).and_return('12')
+
+      expect{frame.roll_1(io)}.to raise_error('error- score higher than remaining pins')
+    end
+  end
+
+
+  context 'method roll_2' do
+    it 'can take io input and score the roll when not a strike' do
+      io = double(:io)
+      expect(io).to receive(:puts).and_return('10 pins remaining')
+      expect(io).to receive(:puts).and_return('Enter score for second roll')
+      expect(io).to receive(:gets).and_return('9')
+      frame.roll_2(io)
+      expect(frame.score2).to eq 9
+    end
+
+    it 'raises error if given impossible roll' do
+      io = double(:io) 
+      expect(io).to receive(:puts).and_return('10 pins remaining')
+      expect(io).to receive(:puts).and_return('Enter score for second roll')
+      expect(io).to receive(:gets).and_return('13')
+      expect{frame.roll_2(io)}.to raise_error('error- score higher than remaining pins')
+      expect(frame.score2).to eq 0
+    end
+
+    it 'updates the remaining pins' do
+      io = double(:io)
+      expect(io).to receive(:puts).and_return('10 pins remaining')
+      expect(io).to receive(:puts).and_return('Enter score for second roll')
+      expect(io).to receive(:gets).and_return('9')
+      frame.roll_2(io)
+      expect(frame.pins).to eq 1
+    end
+
+    it 'sets @bonus to true and @bonus_type to strike if a strike is scored' do
+      io = double(:io)
+      expect(io).to receive(:puts).and_return('10 pins remaining')
+      expect(io).to receive(:puts).and_return('Enter score for second roll')
+      expect(io).to receive(:gets).and_return('10')
+      frame.roll_2(io)
+      expect(frame.bonus).to be true
+      expect(frame.bonus_type).to eq 'strike'
+    end
+
+    it ' sets @bonus to true and @bonus_type to spare if a spare is scored' do
+      io = double(:io)
+      expect(io).to receive(:puts).and_return('10 pins remaining')
+      expect(io).to receive(:puts).and_return('Enter score for first roll')
+      expect(io).to receive(:gets).and_return('7')
+      frame.roll_1(io)
+      io2 = double(:io2)
+      expect(io2).to receive(:puts).and_return('3 pins remaining')
+      expect(io2).to receive(:puts).and_return('Enter score for second roll')
+      expect(io2).to receive(:gets).and_return('3')
+      frame.roll_2(io2)
+      expect(frame.pins).to eq 0
+      expect(frame.bonus).to be true
+      expect(frame.bonus_type).to eq 'spare'
     end
   end
 end
